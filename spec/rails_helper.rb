14273a2b1f8e6d6534b32785461ab8577b17cec2
Mark Sayson <masayson@gmail.com>
2017-04-16 00:34:29 -0700

    Require capybara in rails_helper, require rails_helper in all feature tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 if Rails.env.production?
   abort('The Rails environment is running in production mode!')
 end
+require 'capybara/rspec'
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
